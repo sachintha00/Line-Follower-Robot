0b9763f3ba73924bb04d21433b937e07d6a0554c
sachintha00 <sachintha0603@gmail.com>
2022-01-02 19:26:44 +0530

    write a code for checking single sensor check with analog inputs and digital inputs

---
+++ sensor_check/check_ir.ino
@@ -0,0 +1,28 @@
+#define sensor 3
+#define check 13
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(sensor, INPUT);
+  pinMode(check, OUTPUT);
+
+}
+
+void loop() {
+  //  digitalWrite(check,HIGH);
+  //  delay(1000);
+  //  digitalWrite(check,LOW);
+  //  delay(1000);
+
+  if (digitalRead(sensor) == HIGH) {
+    digitalWrite(check, HIGH);
+//    Serial.print(digitalRead(sensor));
+    Serial.println(analogRead(sensor));
+  }
+  else{
+    digitalWrite(check,LOW);
+//    Serial.print(digitalRead(sensor));
+    Serial.println(analogRead(sensor));
+  }
+
+}
