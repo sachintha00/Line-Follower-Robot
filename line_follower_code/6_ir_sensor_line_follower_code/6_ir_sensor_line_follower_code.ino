edc9e4f55605c31a923eca29acf9ad08719f3351
sachintha00 <sachintha0603@gmail.com>
2022-01-04 21:06:07 +0530

    updated

--- line_follower_code/6_ir_sensor_line_follower_code/6_ir_sensor_line_follower_code.ino
+++ line_follower_code/6_ir_sensor_line_follower_code/6_ir_sensor_line_follower_code.ino
@@ -1,3 +1,11 @@
+//define 6 ir sensor
+#define sensor1 = A0
+#define sensor2 = A1
+#define sensor3 = A2
+#define sensor4 = A3
+#define sensor5 = A4
+#define sensor6 = A5
+
 void setup() {
   // put your setup code here, to run once:
 
