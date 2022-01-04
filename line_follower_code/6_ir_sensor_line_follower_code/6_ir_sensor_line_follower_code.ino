2052edf8978769ee38bc564b8f9b77edfeb1f724
sachintha00 <sachintha0603@gmail.com>
2022-01-04 21:09:10 +0530

    updated

--- line_follower_code/6_ir_sensor_line_follower_code/6_ir_sensor_line_follower_code.ino
+++ line_follower_code/6_ir_sensor_line_follower_code/6_ir_sensor_line_follower_code.ino
@@ -6,6 +6,12 @@
 #define sensor5 = A4
 #define sensor6 = A5
 
+//define motors
+#define rightMotor_R 0
+#define rightMotor_L 0
+#define leftMotor_R 0
+#define leftMotor_L 0
+
 void setup() {
   // put your setup code here, to run once:
 
