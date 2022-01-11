6fdfdcf1537af7581d655ff19dd2788b625d7626
sachintha00 <sachintha0603@gmail.com>
2022-01-11 18:02:24 +0530

    updated

---
+++ Motor Check/check_motor/check_motor.ino
@@ -0,0 +1,17 @@
+//define motors
+#define rightMotor_ena 5
+#define rightMotor_R 6
+#define rightMotor_L 7
+#define leftMotor_R 9
+#define leftMotor_L 8
+#define leftMotor_enb 10
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
