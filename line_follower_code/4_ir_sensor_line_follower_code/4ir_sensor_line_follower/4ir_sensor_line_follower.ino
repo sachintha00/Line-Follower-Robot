0086bfade8028ef5bda3958aa5cbc3785a0746b6
sachintha00 <sachintha0603@gmail.com>
2022-01-11 18:22:26 +0530

    updated

---
+++ line_follower_code/4_ir_sensor_line_follower_code/4ir_sensor_line_follower/4ir_sensor_line_follower.ino
@@ -0,0 +1,23 @@
+//define 4 ir sensor
+#define sensor1  A0
+#define sensor2  A1
+#define sensor3  A2
+#define sensor4  A3
+
+//define motors
+#define rightMotor_ena 5
+#define rightMotor_R 6
+#define rightMotor_L 7
+#define leftMotor_R 9
+#define leftMotor_L 8
+#define leftMotor_enb 10
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
