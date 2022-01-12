b25f22ab42383535e3abe673b62fe6afecacbf8a
sachintha00 <sachintha0603@gmail.com>
2022-01-12 21:31:40 +0530

    updated

---
+++ line_follower_code/bee_ir_linefollower/bee_ir_linefollower.ino
@@ -0,0 +1,17 @@
+#include <BeeLineSensorPro.h>
+
+BeeLineSensorPro sensor = BeeLineSensorPro(
+(unsigned char[]) {
+  A0, A1, A2, A3, A4, A5
+},
+LINE_BLACK);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
