5ecb4c38856dfd4f5a8621add54ce7909a416c40
sachintha00 <sachintha0603@gmail.com>
2022-01-14 20:40:03 +0530

    updated

--- line_follower_code/bee_ir_linefollower/bee_ir_linefollower.ino
+++ line_follower_code/bee_ir_linefollower/bee_ir_linefollower.ino
@@ -1,5 +1,13 @@
 #include <BeeLineSensorPro.h>
 
+//define motors
+#define rightMotor_ena 5
+#define rightMotor_R 6
+#define rightMotor_L 7
+#define leftMotor_R 9
+#define leftMotor_L 8
+#define leftMotor_enb 10
+
 BeeLineSensorPro sensor = BeeLineSensorPro(
 (unsigned char[]) {
   A0, A1, A2, A3, A4, A5
